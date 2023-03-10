f6f07d5b3a0aa75694e106460e447132e348c04a
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-03-10 21:32:17 +0330

    add javascript for displaying popup

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // get description value from description-value element its textarea
 // you will display title value  in title-el element
 // you will display title description  in description-el element
+
 // you going to make a for loop for  class note every time we addNote() its going to generate a note box
 // this function going to add note you write get data and going to display it in screen button addNote()
 // you will get todays date
@@ -16,6 +17,14 @@
 //  you will change popup-text text on editNote()it will be Update a Note and button will be Update a Note
 // when we click setting icon it will add show class to  setting-el element
 // when we click to screan it will remove show class from setting-el element
+let popup = document.querySelector(".popup-box");
+let titleValue = document.getElementById("title-value").value;
+let descriptionValue = document.getElementById("description-value").value;
+let titleEl = document.getElementById("title-el");
+let descriptionEl = document.getElementById("description-el");
+let noteBox = document.getElementById("note-box");
+let note = [];
+
 function showMenu(elem) {
   elem.parentElement.classList.add("show");
   document.addEventListener("click", (e) => {
@@ -23,4 +32,54 @@ function showMenu(elem) {
       elem.parentElement.classList.remove("show");
     }
   });
+}
+
+function addNewNote() {
+  popup.classList.remove("popup-display-none");
+}
+function closePopup() {
+  popup.classList.add("popup-display-none");
+}
+// console.log(descriptionValue);
+// console.log(descriptionValue);
+function addNote() {
+  let noteData = {
+    title: descriptionValue,
+    description: descriptionValue,
+  };
+  // note.push(noteData);
+  // console.log(note);
+  popup.classList.add("popup-display-none");
+  displayNote();
+}
+function displayNote() {
+  let noteBoxData = ``;
+  for (let i = 0; i < note.length; i++) {
+    noteBoxData = `<li class="note">
+          <div class="details">
+            <p id="title-el">${note[i].noteData.title}</p>
+            <span id="description-el"
+              >${note[i].noteData.description}</span
+            >
+          </div>
+          <div class="bottom-content">
+            <span id="note-date-el">April 3, 2022</span>
+            <div class="setting" id="setting-el">
+              <i
+                class="fa-solid fa-ellipsis ellipsis"
+                onclick="${showMenu(this)}"
+              ></i>
+              <ul class="menu">
+                <a href="#" onclick="${deleteNote()}">
+                  <li><i class="fa-solid fa-trash-can"></i>Trash</li>
+                </a>
+                <a href="#" onclick="${editNote()}">
+                  <li><i class="fa-regular fa-pen-to-square"></i>Edit</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </li>
+`;
+  }
 }
